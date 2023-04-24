1d261acda1dc7b7852e537a47e37d18320a7df11
richard wetterauer <rhwette2022@gmail.com>
2023-04-24 08:39:46 -0500

    added Oakley and updated TED

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-// import React from 'react';
 import { Grid, GridItem, Button } from '@chakra-ui/react';
 import Header from './components/Header';
 import Intro from './components/Intro';
 import Problems from './components/Problems';
 import Techniques from './components/Techniques';
-
-import { useState } from 'react';
+import Ted from './components/Ted';
+import Oakley from './components/Oakley';
 
 function App() {
-  const [isVisible, setVisibility] = useState(false);
-
-  const handleClick = () => {
-    console.log(isVisible);
-    setVisibility(true);
-    console.log(isVisible);
-  };
-
-  console.log(isVisible);
-
   return (
     <>
-      <Button onClick={handleClick}>test</Button>
       <Grid
         templateAreas={` "header header"
                               "intro intro"
                               "problems problems"
                               "techniques techniques"
                               "ted ted"
-                              "asideoakley oakley"
+                              "oakleyaside oakleyaside"
                               "feynman feynman"
                               "factsandfigs factsandfigs"
                               "kahan kahan"
@@ -52,18 +40,18 @@ function App() {
           <Techniques />
         </GridItem>
 
-        <GridItem area="ted" bg="pink">
-          ted
+        <GridItem area="ted" bg="white">
+          <Ted />
         </GridItem>
-        <GridItem area="asideoakley" bg="orange">
-          asideoakley
+
+        <GridItem area="oakleyaside" bg="orange">
+          <Oakley />
         </GridItem>
-        <GridItem area="oakley" bg="dodgerblue">
-          oakley
-        </GridItem>
+
         <GridItem area="feynman" bg="green">
           feynman
         </GridItem>
+
         <GridItem area="factsandfigs" bg="blue">
           factsandfigs
         </GridItem>
